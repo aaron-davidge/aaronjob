9297c4d467b687235699682e3228aed072d7d43b
Aaron Davidge <aarondavidge@Aarons-MBP.lan>
2021-08-10 15:11:56 -0500

    commit

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
-ini_set('display_errors', 1);
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-
-define (DB_USER, "root");
-define (DB_PASSWORD, "root");
-define (DB_DATABASE, "sole");
-define (DB_HOST, "localhost");
-
-
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
